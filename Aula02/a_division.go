de65f7998c89d760bd96baae46789889eb5d4df9
Caio2a7 <caiodanielfonseca@gmail.com>
2026-08-29 11:57:44 -0300

    feat: solve problem A division in Aula02

--- Aula02/a_division.go
+++ Aula02/a_division.go
@@ -1,8 +1,26 @@
 package main
 
+import "fmt"
 
 func main(){
-	
+	var cases int
+	fmt.Scan(&cases)
+	for range cases{
+		var input int
+		fmt.Scan(&input)
+		var division int
+		switch {
+			case input >= 1900:
+				division = 1
+			case input >= 1600:
+				division = 2
+			case input >= 1400:
+				division = 3
+			default:
+				division = 4
+		}
+		fmt.Printf("Division %d\n", division)
+	}
 
 
 }
