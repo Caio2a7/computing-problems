4c3a2241786c031ce257bfc2caf49d734ab17b21
Caio2a7 <caiodanielfonseca@gmail.com>
2026-08-29 12:01:09 -0300

    fix: update team formation loop in Aula03 e_perfect_team

--- Aula03/e_perfect_team.go
+++ Aula03/e_perfect_team.go
@@ -1,34 +1,32 @@
 package main
-
 import "fmt"
-
 func main(){
 	var cases int
 	fmt.Scan(&cases)
-		
+
 	for range cases{
 		perfect := 0
-
 		coders, maths, any := 0,0,0
 		fmt.Scan(&coders)
 		fmt.Scan(&maths)
 		fmt.Scan(&any)
 		for coders > 0 && maths > 0 {
-			if any <= 0 && coders > 1 && coders >= maths{
-				any++
-				coders--
-			} else if any <= 0 && maths > 1 && maths > coders{
-				any++
-				maths--
+			if any <= 0 {
+				if coders > 1 && coders >= maths{
+					any++
+					coders--
+				} else if maths > 1 && maths > coders{
+					any++
+					maths--
+				} else {
+					break
+				}
 			}
 			coders--
 			maths--
 			any--
 			perfect++
-			
-
 		}
 		fmt.Printf("%d\n", perfect)
 	}
 }
-
